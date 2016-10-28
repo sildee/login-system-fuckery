d36a5a17afddf4df69b45e0299072a9443171ce9
sildee <silasjuh@live.nl>
2016-10-28 17:30:30 +0200

    Add files via upload

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,13 +1,34 @@
- <?php
+<?php
  	session_start();
 
 	include '../dbh.php';
  
 	$username = $_POST['uid'];
 	$password = $_POST['upw'];  
- 
-	$sql = "INSERT INTO user (`uid`, `upw`) 
-					VALUES ('$username', '$password')";
-	$result = mysqli_query($conn, $sql);  
+	
+	if (empty('uid')){
+		header("Location: ../registration.php?error=empty");	
+		exit();		
+	}
+	if (empty('upw')){
+		header("Location: ../registration.php?error=empty");	
+		exit();		
+	}
 
-	header("Location: ../index.php");
+    else {
+		$sql = "SELECT uid FROM user WHERE uid='$username'";
+		$result = mysqli_query($conn, $sql);
+		$uidcheck = mysqli_num_rows($result);
+	}
+	if($uidcheck > 0) {
+		header("Location: ../registration.php?error=duplicate_username");	
+		exit();	
+	} 	else {
+		$sql = "INSERT INTO user (`uid`, `upw`) 
+				VALUES ('$username', '$password')";
+		$result = mysqli_query($conn, $sql);  
+
+		header("Location: ../index.php");
+	}
+
+
