fd071a21a7a87dfef2ff9ec600390a3b9c5e3808
sildee <silasjuh@live.nl>
2016-11-01 20:45:11 +0100

    Add files via upload

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -4,14 +4,27 @@
  
 	$username = $_POST['uid'];
 	$password = $_POST['upw'];  
- 
-	$sql = "SELECT * FROM user WHERE uid='$username' AND upw='$password'";
+
+
+	$sql = "SELECT * FROM user WHERE uid='$uid'";
 	$result = mysqli_query($conn, $sql);  
+	$row= mysqli_fetch_assoc($result);
+	$hash_pwd = $row['upw'];
+	$hash = password_verify($password, $hash_pwd);
 
-	if(!$row = mysqli_fetch_assoc($result)) {
-		echo "Your username or password is incorrect!";
+	if ($hash == 0) {
+		header("Location: ../registration.php?error=hash");
+		exit();
 	} else {
-		$_SESSION['id'] = $row['id'];
-	}
 
-	header("Location: ../index.php");
+ 		$sql = "SELECT * FROM user WHERE uid='$username' AND upw='$hash_pwd'";
+		$result = mysqli_query($conn, $sql);  
+
+		if(!$row = mysqli_fetch_assoc($result)) {
+			echo "Your username or password is incorrect!";
+		} else {
+			$_SESSION['id'] = $row['id'];
+		}
+
+		header("Location: ../index.php");
+	}
--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -19,16 +19,16 @@
 		$sql = "SELECT uid FROM user WHERE uid='$username'";
 		$result = mysqli_query($conn, $sql);
 		$uidcheck = mysqli_num_rows($result);
-	}
-	if($uidcheck > 0) {
-		header("Location: ../registration.php?error=duplicate_username");	
-		exit();	
+		if($uidcheck > 0) {
+			header("Location: ../registration.php?error=duplicate_username");	
+			exit();	
 	} 	else {
+		$upwhash = password_hash($password, PASSWORD_DEFAULT);
 		$sql = "INSERT INTO user (`uid`, `upw`) 
-				VALUES ('$username', '$password')";
+				VALUES ('$username', '$upwhash')";
 		$result = mysqli_query($conn, $sql);  
 
 		header("Location: ../index.php");
 	}
-
+	}
 
