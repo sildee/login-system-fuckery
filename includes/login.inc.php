49668b142465419956f8bf7a8072d90cc57d4d52
sildee <silasjuh@live.nl>
2016-11-07 18:12:16 +0100

    Add files via upload

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -9,15 +9,15 @@
 	$sql = "SELECT * FROM user WHERE uid='$uid'";
 	$result = mysqli_query($conn, $sql);  
 	$row= mysqli_fetch_assoc($result);
-	$hash_pwd = $row['upw'];
-	$hash = password_verify($password, $hash_pwd);
+	$hash_upw = $row['upw'];
+	$hash = password_verify($password, $hash_upw);
 
 	if ($hash == 0) {
-		header("Location: ../registration.php?error=hash");
+		header("Location: ../index.php?error=hash");
 		exit();
 	} else {
 
- 		$sql = "SELECT * FROM user WHERE uid='$username' AND upw='$hash_pwd'";
+ 		$sql = "SELECT * FROM user WHERE uid='$username' AND upw='$hash_upw'";
 		$result = mysqli_query($conn, $sql);  
 
 		if(!$row = mysqli_fetch_assoc($result)) {
